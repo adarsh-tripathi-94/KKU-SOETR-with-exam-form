a0ce0ebbf902bef0c51d4c7d1e4ba4e8611cc742
adarsh-tripathi-94 <ramayanyug@gmail.com>
2026-03-22 12:47:39 +0530

    Added supabase url and anon key

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,10 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://nfytxnqdlkfnvdzzfxil.supabase.co"; 
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5meXR4bnFkbGtmbnZkenpmeGlsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQwMDgzNDUsImV4cCI6MjA4OTU4NDM0NX0.XuF2UPzAn-u1LyhvU7hRMfyQw97hwpR6Zw-f_j3vz3U";
 
 if (!supabaseUrl || !supabaseAnonKey) {
-  console.error("Critical Error: Supabase connection keys are missing from .env.local");
+  console.error('Critical Error: Supabase connection keys are missing.');
 }
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
